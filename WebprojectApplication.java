d0ce85585eb039d5521c6c0bd34e59d6f20cd7b0
Ila-varasan <146458530+Ila-varasan@users.noreply.github.com>
2024-08-27 13:02:41 +0530

    Add files via upload

---
+++ WebprojectApplication.java
@@ -0,0 +1,8 @@
+spring.application.name=webproject
+spring.datasource.url=jdbc:mysql://localhost:3306/Finalproject
+spring.datasource.username=root
+spring.datasource.password=Msila@123
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+
+# JPA and Hibernate Configuration
+spring.jpa.hibernate.ddl-auto=update
